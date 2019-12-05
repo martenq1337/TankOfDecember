8d8645b74f6517b0a6a6231f7c9b2ef75b6c49ca
Laszlo Bajusz <martenq1337@gmail.com>
2019-12-05 20:17:00 +0100

    Added: rigidbody to tank and map, controller script

---
+++ Assets/Scripts/Tank/Controller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    Rigidbody _RigidBody;
+    private float _Speed = 3.0f;
+
+    private void Start()
+    {
+        _RigidBody = this.GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(Vector3.forward * Time.deltaTime);
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(Vector3.back* Time.deltaTime);
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(Vector3.left* Time.deltaTime);
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(Vector3.right * Time.deltaTime);
+    }
+}
